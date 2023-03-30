fb2df5a2fc12ee5b8418d8c4de09a3460941e7f6
Timothy Borunov <timobohr@bu.edu>
2023-03-30 09:08:35 -0400

    Preemptive submit

--- client.py
+++ client.py
@@ -1,3 +1,34 @@
+
+import sys
+import socket
+import selectors
+import types
+import pdb 
+
+
+def start_connections(host, port, num_conns):
+    sel = selectors.DefaultSelector()
+    messages1 = [b"Message 1 from client.", b"Message 2 from client."]
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print(f"Starting connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=messages1.copy(),
+            outb=b"",
+        )
+        sel.register(sock, events, data=data)
+
+host, port, num = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+start_connections(host,port,num)
+"""
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
@@ -9,3 +40,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data = s.recv(1024)
 
 print(f"Received {data!r}")
+"""
--- server.py
+++ server.py
@@ -1,5 +1,67 @@
-import socket
 
+
+import sys
+import socket
+import selectors
+import types
+import pdb
+
+sel = selectors.DefaultSelector()
+
+# ...
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    pdb.set_trace()
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            print(f"Received {recv_data!r} from connection {data.connid}")
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
+            print(f"Closing connection {data.connid}")     
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print(f"Sending {data.outb!r} to connection {data.connid}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+    
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
+
+    
+"""
 HOST = "127.0.0.1"
 
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -15,3 +77,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 break
             conn.sendall(data)
+"""
